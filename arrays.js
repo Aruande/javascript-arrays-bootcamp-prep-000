0c096ab9f7b38b6007b6f8aad905c86d4d4430e9
Marrio Hinkle <kalunga212@gmail.com>
2018-11-11 10:10:00 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars= ["snickers","hundred grand","kitkat","skittles"];
+  var moreCandy = "twix"
+function addElementToBeginningOfArray(chocolateBars, moreCandy){
+  return [moreCandy, ...chocolateBars];
+}
+
+ 
