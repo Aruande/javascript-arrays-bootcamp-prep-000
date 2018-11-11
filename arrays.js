ff7f4af06e3311dd151a2f010180dad085197365
Marrio Hinkle <kalunga212@gmail.com>
2018-11-11 10:52:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,52 @@
-function accessElementInArray(array, index) {
+r chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+ 
+function addElementToBeginningOfArray(chocolateBars, moreCandy){
+  return [moreCandy, ...chocolateBars];
+}
+ 
+function destructivelyAddElementToBeginningOfArray(chocolateBars, moreCandy){
+  chocolateBars.unshift(moreCandy);
+  return chocolateBars;
+}
+ 
+function addElementToEndOfArray(chocolateBars, moreCandy){
+  return [...chocolateBars, moreCandy];
+}
+ 
+function destructivelyAddElementToEndOfArray(chocolateBars, moreCandy){
+  chocolateBars.push(moreCandy);
+  return chocolateBars;
+}
+ 
+function accessElementInArray(array, index){
   return array[index];
 }
+ 
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  chocolateBars.shift();
+  return chocolateBars;
+}
+ 
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1);
+}
+ 
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop();
+  return chocolateBars;
+}
+ 
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1);
+}
+
+
+
+
+
+
+
+
+
+
